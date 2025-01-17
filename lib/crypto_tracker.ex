a13e8b028f7c0e2e1b196a262afe665770f539a4
William Martin <maartz@icloud.com>
2025-01-17 21:02:46 +0100

    first commit

---
+++ lib/crypto_tracker.ex
@@ -0,0 +1,18 @@
+defmodule CryptoTracker do
+  @moduledoc """
+  Documentation for `CryptoTracker`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CryptoTracker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/crypto_tracker_test.exs
@@ -0,0 +1,8 @@
+defmodule CryptoTrackerTest do
+  use ExUnit.Case
+  doctest CryptoTracker
+
+  test "greets the world" do
+    assert CryptoTracker.hello() == :world
+  end
+end
